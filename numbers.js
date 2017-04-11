3944793be5d36b3a50efd4218800020df1b029ed
Herminio Garcia <hermano360@gmail.com>
2017-04-11 01:43:57 -0700

    added more people

--- numbers.js
+++ numbers.js
@@ -1,6 +1,9 @@
 var contacts = [
 	{name: 'Herminio',number:"7655436533"},
-	{name: 'Alberto Casteneda',number:"6265332549"}
+	{name: 'Alberto Casteneda',number:"6265332549"},
+	{name: 'Lisa Cumes',number:"3107747011"},
+	{name: 'CJ',number:"6268066335"}
+
 ];
 
 module.exports=  function(){
